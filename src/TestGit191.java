d601e5e845a92eeacc43b1d40744727b6ee66b1b
Jinxiaobai512 <545743523@qq.com>
2021-12-15 18:23:16 +0800

    Initial commit

---
+++ src/TestGit191.java
@@ -0,0 +1,15 @@
+
+public class TestGit191 {
+    public static void main(String[] args) {
+
+        //同学们将自己的学号姓名当做参数，调用stuInfo函数，进行协同开发
+
+    }
+
+    public static void stuInfo(String sid, String sname) {
+        System.out.println("学号：" + sid + "姓名: " + sname);
+    }
+}
+
+
+
