c47445840b1110df69c20adde1b8d336c11d6272
Jinxiaobai512 <545743523@qq.com>
2021-12-15 21:40:46 +0800

    王五的修改 将自己的代码修改，并将自己的与之前的代码变动congflict解决

--- src/TestGit191.java
+++ src/TestGit191.java
@@ -5,6 +5,7 @@ public class TestGit191 {
         //同学们将自己的学号姓名当做参数，调用stuInfo函数，进行协同开发
         stuInfo("20199001","李四");
         //下面接龙同学们自己的代码修改
+        stuInfo("20199002","王五");
     }
 
     public static void stuInfo(String sid, String sname) {
